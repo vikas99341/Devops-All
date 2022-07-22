bd46444087754185d9eaa105992427bdee13d322
vikas99341 <vikasisinlove@gmail.com>
2022-07-22 17:44:57 +0530

    Devops final

---
+++ Jenkins/sonar qube/FirstSonarJava.java
@@ -0,0 +1,24 @@
+package com.muthu4all.sonarqube;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FirstSonarJava {
+
+	public static final String abc="";
+	public static String [] strings1 = {"first","second"};
+	
+	
+	public static void main(String[] args) {
+		String MayBeNull = null;
+		System.out.println("Hello SonarQube "+MayBeNull);
+		 
+		for (int i = 10; i < 15; i++) { 
+			System.out.println("Loop not true even once");
+		}
+			
+	}
+	
+
+
+}
---
+++ Jenkins/sonar qube/sonar-project.properties
@@ -0,0 +1,15 @@
+sonar.host.url=http://localhost:9000
+#----- Default source code encoding
+sonar.sourceEncoding=UTF-8
+
+sonar.projectKey=java-sonar-scanner-simple-2
+sonar.projectName=Simple 2 Java Project analyzed with the SonarQube Scanner
+sonar.projectVersion=1.0
+
+sonar.scm.disabled=True
+
+sonar.sources=src
+
+sonar.language=java
+
+sonar.sourceEncoding=UTF-8
